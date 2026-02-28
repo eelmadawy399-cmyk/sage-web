63ba7739fe8e1ce8673acb0a4b69564d54341c5b
SAGE Egypt <sage@egypt.app>
2026-02-28 18:27:29 +0200

    تحديث التقرير ونسخة الموبايل وتفريغ الكاش

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'sage-egypt-v1';
+const CACHE_NAME = 'sage-egypt-v2';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
@@ -12,9 +12,24 @@ const ASSETS_TO_CACHE = [
 ];
 
 self.addEventListener('install', (event) => {
+    self.skipWaiting();
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => {
             return cache.addAll(ASSETS_TO_CACHE);
+        })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
         })
     );
 });
